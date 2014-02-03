2279dbde1de340266364a7ad181212c586c5b30d
Lagspoon <lagspoon@gmail.com>
2014-02-03 12:24:48 +0100

    Initial Commit

---
+++ writingTool/WTViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTViewController.h
+//  writingTool
+//
+//  Created by Olivier Delecueillerie on 03/02/2014.
+//  Copyright (c) 2014 Olivier Delecueillerie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTViewController : UIViewController
+
+@end
---
+++ writingTool/WTViewController.m
@@ -0,0 +1,29 @@
+//
+//  WTViewController.m
+//  writingTool
+//
+//  Created by Olivier Delecueillerie on 03/02/2014.
+//  Copyright (c) 2014 Olivier Delecueillerie. All rights reserved.
+//
+
+#import "WTViewController.h"
+
+@interface WTViewController ()
+
+@end
+
+@implementation WTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
